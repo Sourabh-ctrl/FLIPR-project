60244dc8df1f1fa8eca4f16839ad3fe2b9678b9b
sourabh-ctrl <lathisaurav@gmail.com>
2025-12-19 23:15:55 +0530

    some issues

--- admin/src/pages/AdminPanel.jsx
+++ admin/src/pages/AdminPanel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProjectManager from '../components/Projects';
-import ClientManager from '../components/clients';
+import ClientManager from '../components/Clients';
 import { ContactViewer, SubscriberViewer } from '../components/Subscribe';
 
 const AdminPanel = () => {
