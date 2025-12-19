8a47ee8ccfddf15ee95419a945192aa313925bac
sourabh-ctrl <lathisaurav@gmail.com>
2025-12-19 23:02:51 +0530

    commit

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,12 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [react() ,  tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    },
     proxy: {
       '/api': {
-        target: 'https://flipr-project-qhzj.onrender.com',
+        target: 'https://flipr-project-qhzj.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
