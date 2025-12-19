64baa9247da277200659769bcb578a3fc08c8d76
sourabh-ctrl <lathisaurav@gmail.com>
2025-12-19 23:17:33 +0530

    some fixes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     cors: true,
-    // headers: {
-    //   'Access-Control-Allow-Origin': '*',
-    //   'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    //   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    // },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    },
     proxy: {
       '/api': {
         target: 'https://flipr-project-qhzj.onrender.com/',
